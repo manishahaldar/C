0d6dfc229e0d6a34d1688ecc948e2b171e6a203d
manishahaldar <116336962+manishahaldar@users.noreply.github.com>
2023-03-14 20:22:05 +0530

    Add files via upload

---
+++ sum.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+void main(){
+    int a,b,c;
+    printf("Enter Number A : ");
+    scanf("%d",&a);
+    printf("Enter Number B : ");
+    scanf("%d",&b);
+    c=a+b;
+    printf("Sum is : %d",c);
+    getch();
+}
